942b892d735437e9013ee156668fd73f613de06b
Svetoslav Denev <svetoslav.denev@lecturio.de>
2019-04-03 21:18:27 +0200

    tiny fix for a style

--- android/app/src/main/java/com/aquauikit/MainApplication.java
+++ android/app/src/main/java/com/aquauikit/MainApplication.java
@@ -3,6 +3,7 @@ package com.aquauikit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.cmcewen.blurview.BlurViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BlurViewPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
       );
